142fcc29f7ea3c80d6e6ecab709b683f874b3060
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-02-11 17:41:05 +0200

    styling hero component and adding it to home page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,13 @@
 import React from "react";
 
-export default function Hero() {
-  return <h1>hello from hero</h1>;
+export default function Hero({ children }) {
+  return (
+    <div className="hero">
+      <div className="banner">
+        <h1>Biggest collection of books under one roof</h1>
+        <p>find what interests you</p>
+        {children}
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,15 @@
 import React from "react";
+import Hero from "../components/Hero";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <h1>hello from home page</h1>;
+  return (
+    <>
+      <Hero>
+        <Link to="/products" className="btn btn-primary btn-hero">
+          Our products
+        </Link>
+      </Hero>
+    </>
+  );
 }
