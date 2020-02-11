eb8f1f43ad67d074d2df89a14363b6a76fe1c5ca
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-02-11 14:57:56 +0200

    Adding Browser router to App component with switch and Routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import About from "./pages/About";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Error from "./pages/Error";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Products from "./pages/Products";
+import ProductDetails from "./pages/ProductDetails";
 
 export default function App() {
-  return <h1>hello from app component</h1>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/cart" component={Cart} />
+        <Route component={Error} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
