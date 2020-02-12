8b3ce4634c59390ab6095a28654856c45102cccb
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-02-12 13:02:00 +0200

    implementing CartItem component

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,5 +1,39 @@
 import React from "react";
+import { FaAngleUp, FaAngleDown } from "react-icons/fa";
+import { CartContext } from "../../context/cart";
 
-export default function CartItem() {
-  return <h1>hello form cart item</h1>;
+export default function CartItem({ id, image, title, price, amount }) {
+  return (
+    <article className="cart-item">
+      <img src={image} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h5>${price}</h5>
+        <button
+          onClick={() => {}}
+          type="button"
+          className="cart-btn remove-btn"
+        >
+          remove
+        </button>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="cart-btn amount-btn"
+          onClick={() => {}}
+        >
+          <FaAngleUp />
+        </button>
+        <p className="item-amount">{amount}</p>
+        <button
+          type="button"
+          className="cart-btn amount-btn"
+          onClick={() => {}}
+        >
+          <FaAngleDown />
+        </button>
+      </div>
+    </article>
+  );
 }
--- src/context/cart.js
+++ src/context/cart.js
@@ -8,6 +8,15 @@ function CartProvider({ children }) {
   const [total, setTotal] = useState(0);
   const [cartItems, setCartItems] = useState(0);
 
+  const removeItem = id => {};
+
+  const increaseAmount = id => {};
+  const decreaseAmount = id => {};
+
+  const addToCart = product => {};
+
+  const clearCart = () => {};
+
   return (
     <CartContext.Provider value={{ cart, total, cartItems }}>
       {children}
