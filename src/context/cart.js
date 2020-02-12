0d3ba8bd7bdcb9548da4c05d724646553df815cd
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-02-12 13:11:17 +0200

    adding useEffect hook in cart context to calculate cartItems and totalPrice on cart change

--- src/context/cart.js
+++ src/context/cart.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import localCart from "../utils/localCart";
 
 const CartContext = React.createContext();
@@ -7,6 +7,18 @@ function CartProvider({ children }) {
   const [cart, setCart] = useState(localCart);
   const [total, setTotal] = useState(0);
   const [cartItems, setCartItems] = useState(0);
+
+  useEffect(() => {
+    //local storage
+    let newCartItems = cart.reduce((acc, item) => {
+      return (acc += item.amount);
+    }, 0);
+    setCartItems(newCartItems);
+    let newTotal = cart.reduce((acc, item) => {
+      return (acc += item.amount * item.price);
+    }, 0);
+    setTotal(newTotal);
+  }, [cart]);
 
   const removeItem = id => {};
 
