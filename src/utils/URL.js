de50e19ed470883d5f207296707aa6ce41dbb7ce
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-02-13 22:17:33 +0200

    change in url address -> now working with heroku production app

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,4 +1,4 @@
 // url
-export default "http://localhost:3001";
+export default "https://react-bookstore-api.herokuapp.com";
 
 // "https://cors-anywhere.herokuapp.com/http://johnsmilgatutorials.com/projects/react-tech-store-v2"
