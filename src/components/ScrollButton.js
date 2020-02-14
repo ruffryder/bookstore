3d977912600b0ab91e5e697a1318d52465b08a76
Christian Dimitrov <christian_dimitrov@abv.bg>
2020-02-14 12:14:25 +0200

    adding height property to UserContext state and using it to listen for scroll event

--- src/components/ScrollButton.js
+++ src/components/ScrollButton.js
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { FaAngleDoubleUp } from "react-icons/fa";
 import { UserContext } from "../context/user";
 
 function ScrollButton() {
-  const height = 200;
+  const { height } = useContext(UserContext);
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
--- src/context/user.js
+++ src/context/user.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 const UserContext = createContext();
 function getUserFromLocalStorage() {
@@ -9,6 +9,14 @@ function getUserFromLocalStorage() {
 
 function UserProvider({ children }) {
   const [user, setUser] = useState(getUserFromLocalStorage());
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      setHeight(window.pageYOffset);
+    });
+    return () => window.removeEventListener("scroll", () => {});
+  });
   const userLogin = user => {
     setUser(user);
     localStorage.setItem("user", JSON.stringify(user));
@@ -30,7 +38,15 @@ function UserProvider({ children }) {
   };
   return (
     <UserContext.Provider
-      value={{ user, userLogin, userLogout, alert, showAlert, hideAlert }}
+      value={{
+        user,
+        userLogin,
+        userLogout,
+        alert,
+        showAlert,
+        hideAlert,
+        height
+      }}
     >
       {children}
     </UserContext.Provider>
